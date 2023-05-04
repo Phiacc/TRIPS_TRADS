1575445e3151e1025e952c54fe2c46494fd419d2
Tracey Philips <79120313+Phiacc@users.noreply.github.com>
2023-05-04 09:05:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const slider = [
+    {
+        sliderimages:"./assets/Mask group (2).png"    
+    },
+    {
+        sliderimages:"./assets/heroimage.png"
+    },
+]
+console.log(slider)
+
+const images = document.querySelector(".slider-image")
+
+for (let i = 0; i < slider.length;i++){
+    const sliderimage = document.createElement("back-drop")
+    sliderimage.src = slider[i].sliderimages
+    images.appendChild(sliderimage)
+    sliderimage.classList.add("back-drop")
+    // clientNames.classList.add("client-name")
+
+}
+
+let counter= 0
+    let variable = setInterval(carousel,3000)
+    function carousel(){
+    let imageLogo= slider.length[counter];
+    images.innerHTML = slider[counter].sliderimages;
+    counter++;
+      if(counter===slider.length){
+          counter=0
+      }
+
+   }
