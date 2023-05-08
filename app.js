0b5ded92e906046cbc70a57e55ce44984b50b3c2
Tracey Philips <79120313+Phiacc@users.noreply.github.com>
2023-05-08 15:48:37 -0500

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const slider = [
 //     {
 //         sliderimages:"./assets/Mask group (2).png"    
@@ -32,37 +31,3 @@
 
 //    }
 
-=======
-const slider = [
-    {
-        sliderimages:"./assets/Mask group (2).png"    
-    },
-    {
-        sliderimages:"./assets/heroimage.png"
-    },
-]
-console.log(slider)
-
-const images = document.querySelector(".slider-image")
-
-for (let i = 0; i < slider.length;i++){
-    const sliderimage = document.createElement("back-drop")
-    sliderimage.src = slider[i].sliderimages
-    images.appendChild(sliderimage)
-    sliderimage.classList.add("back-drop")
-    // clientNames.classList.add("client-name")
-
-}
-
-let counter= 0
-    let variable = setInterval(carousel,3000)
-    function carousel(){
-    let imageLogo= slider.length[counter];
-    images.innerHTML = slider[counter].sliderimages;
-    counter++;
-      if(counter===slider.length){
-          counter=0
-      }
-
-   }
->>>>>>> c4079c242287a5bb3b156110c8bf62568a87849f
